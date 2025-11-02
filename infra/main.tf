b85dd0995c4e55c691132a9e1db536b85853508b
Nitin <nitinbansod19@gmail.com>
2025-11-02 01:29:17 -0500

    Fix deprecated S3 website block and CloudFront restrictions block

--- infra/main.tf
+++ infra/main.tf
@@ -14,10 +14,21 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "site_bucket" {
-  bucket        = var.bucket_name
-  force_destroy = true
-  website {
-    index_document = "index.html"
+  bucket = var.bucket_name
+  tags = {
+    Name = var.bucket_name
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "site_config" {
+  bucket = aws_s3_bucket.site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
   }
 }
 
@@ -48,30 +59,40 @@ resource "aws_s3_bucket_policy" "allow_public_access" {
 resource "aws_cloudfront_origin_access_identity" "this" {}
 
 resource "aws_cloudfront_distribution" "cdn" {
-  origin {
-    domain_name = aws_s3_bucket.site_bucket.bucket_regional_domain_name
-    origin_id   = "s3-origin"
-  }
-
   enabled             = true
-  is_ipv6_enabled     = true
   default_root_object = "index.html"
 
+  origin {
+    domain_name = aws_s3_bucket.site_bucket.bucket_regional_domain_name
+    origin_id   = "s3origin"
+  }
+
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "s3-origin"
     viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "s3origin"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
   }
 
   viewer_certificate {
     cloudfront_default_certificate = true
   }
 
-  logging_config {
-    include_cookies = false
-    bucket          = aws_s3_bucket.logs.bucket_domain_name
-    prefix          = "cdn-logs/"
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  tags = {
+    Environment = var.env
   }
 }
 
